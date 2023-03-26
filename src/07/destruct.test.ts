85dcb8f385fe5106c228a0e77e891699b397f41e
listopadgeorgii <listopadiogl@gmail.com>
2023-03-26 23:03:14 +0300

    07-rewrite 1st tests

--- src/07/destruct.test.ts
+++ src/07/destruct.test.ts
@@ -1,50 +1,89 @@
-import {Man2Type} from "./Desctructuring";
 
-
-let props : Man2Type;
-beforeEach(()=>{
-    props = {
-        name : 'Dimych',
+test('',()=> {
+    let props = {
+        name: 'Dimych',
         age: 32,
-        lessons : [{title: '1'}, {title: '2'}, {title: '3', name: 'react'}],
+        lessons : [{title: '1'}, {title: '2'}],
         address : {
-            street : {
-                title: 'Nezavisimosti street'
-            }
-        }
-
+             street : {
+                 title: 'Nezavisimosti street'
+             }
+         }
     }
-})
 
 
-test('', ()=> {
+    //const age = props.age
+    //const lessons = props.lessons
 
-    // const age = props.age
-    // const lessons = props.lessons
+    const {age, lessons}= props
 
-    const {age, lessons} = props
     const {title} = props.address.street
+
+    const a = props.age
+    const l = props.lessons
 
 
     expect(age).toBe(32)
-    expect(lessons.length).toBe(3)
+    expect(a).toBe(32)
+    expect(lessons.length).toBe(2)
+    expect(l.length).toBe(2)
+
     expect(title).toBe('Nezavisimosti street')
 })
 
 
 
-test('b', ()=> {
-    const l1 = props.lessons[0]
-    const l2 = props.lessons[1]
 
-    const [,ls2, ...restLessons] = props.lessons
-    expect(restLessons.length).toBe(1)
 
-    expect(l1.title).toBe('1')
-    expect(l2.title).toBe('2')
-    // expect(ls1.title).toBe('1')
-    // expect(ls2.title).toBe('2')
-    expect(ls2.title).toBe('2')
-    expect(restLessons[0].name).toBe('react')
-    expect(restLessons[0]).toStrictEqual({title: '3', name: 'react'})
-})
+
+
+// import {Man2Type} from "./Desctructuring";
+//
+//
+// let props : Man2Type;
+// beforeEach(()=>{
+//     props = {
+//         name : 'Dimych',
+//         age: 32,
+//         lessons : [{title: '1'}, {title: '2'}, {title: '3', name: 'react'}],
+//         address : {
+//             street : {
+//                 title: 'Nezavisimosti street'
+//             }
+//         }
+//
+//     }
+// })
+//
+//
+// test('', ()=> {
+//
+//     // const age = props.age
+//     // const lessons = props.lessons
+//
+//     const {age, lessons} = props
+//     const {title} = props.address.street
+//
+//
+//     expect(age).toBe(32)
+//     expect(lessons.length).toBe(3)
+//     expect(title).toBe('Nezavisimosti street')
+// })
+//
+//
+//
+// test('b', ()=> {
+//     const l1 = props.lessons[0]
+//     const l2 = props.lessons[1]
+//
+//     const [,ls2, ...restLessons] = props.lessons
+//     expect(restLessons.length).toBe(1)
+//
+//     expect(l1.title).toBe('1')
+//     expect(l2.title).toBe('2')
+//     // expect(ls1.title).toBe('1')
+//     // expect(ls2.title).toBe('2')
+//     expect(ls2.title).toBe('2')
+//     expect(restLessons[0].name).toBe('react')
+//     expect(restLessons[0]).toStrictEqual({title: '3', name: 'react'})
+// })
