a61c3b9dae95d759b19133ad74423fffc35b6e6d
listopadgeorgii <listopadiogl@gmail.com>
2024-01-02 23:48:35 +0300

    Promise.resolve | Promise.reject

---
+++ src/1111.ts
@@ -0,0 +1,10 @@
+
+
+
+export const resolvedPromise = Promise.resolve(100)
+
+console.log(resolvedPromise)
+ const rejectedPromise = Promise.reject({reason: false})
+
+console.log(resolvedPromise)
+
