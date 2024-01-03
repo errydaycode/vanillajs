7c2f8f38939c23f505f42a9b7aeafc1c0c2b3431
listopadgeorgii <listopadiogl@gmail.com>
2024-01-03 23:00:47 +0300

    14 - JS/TS for students - Promises

--- src/1111.ts
+++ src/1111.ts
@@ -1,10 +1,5 @@
 
 
 
-export const resolvedPromise = Promise.resolve(100)
 
-console.log(resolvedPromise)
- const rejectedPromise = Promise.reject({reason: false})
-
-console.log(resolvedPromise)
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,67 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {log} from "util";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
 function App() {
+
+  //  const resolvedPromise = Promise.resolve(100)
+  //
+  //   resolvedPromise
+  //      .then(data=>  console.log(data) )
+  //      .catch(error =>  console.warn(error))
+  //
+  //
+  // const rejectedPromise = Promise.reject({message: 'some error'})
+  //
+  //
+  //
+  // rejectedPromise
+  //     .then(data=>  console.log(data) )
+  //     .catch(error =>  console.warn(error))
+
+
+  function wait(ms: number) {
+    return new Promise<void>(resolve=> {
+        setTimeout(()=> {
+          resolve()
+        }, ms)
+    })
+  }
+
+  async function run () {
+    await wait(500)
+    console.log(1)
+    await wait(500)
+    console.log(2)
+    await wait(500)
+    console.log(3)
+  }
+
+  run()
+
+  const usersAPI = {
+     getAllUsers(){
+       return Promise.resolve([{name: 'A'} , {name: 'N'}])
+     },
+     login(login: string, password: string) {
+       if(login === '111' && password === '123') {
+         return Promise.reject('incorrect data')
+       } else {
+         return  Promise.resolve('welcome to VK.com!')
+       }
+     }
+  }
+
+  usersAPI.getAllUsers().then(data => console.log(data))
+
+  usersAPI.login('111', '12345')
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+
+
   const names = ["Dimych", "Sveta", "Ivan", "Daniil", "Ignat", "Viktor"]
   const users = [
     {name: "Dimych"},
