3cb0b0a04d34b6e5efc32f3d803713648a0c0f98
listopadgeorgii <listopadiogl@gmail.com>
2024-01-30 23:19:56 +0300

    09 - Sprint 03, JS 01 (Programming Patterns, publisher/subscriber)

--- src/wed_native/js9.js
+++ src/wed_native/js9.js
@@ -113,3 +113,64 @@ async function second() {
 
 first();
 second();
+
+
+//------(Programming Patterns, publisher/subscriber) -  то, что мы хотим сделать в какой-то момент времени,
+// но мы не знаем когда это произойдет// - это всё относится к этому паттерну
+//по этому паттерну мы всегда подписываемся на событие
+// всегда передается подписчик ( ф-ия коллБэк)
+// событие всегда в прошедшем времени, т.е. оно всегда уже произошло
+// на кнпоке мы подписываемся на клик, но он уже должен произойти, чтобы подписчик вызвался
+
+// мы всегда добавляем подписчика, и этот подписчик чего-то ждет
+
+button.addEventListener('click', function subscriber() {} ) // произошел клик - конкретное событие
+setTimeout(function handler() {}, 1000) // прошла секунда - конкретное событие
+store.subscribe( function listener() {} ) // в сторе может произойти только одно событие - изменение стейта,
+// поэтому передаем только подписчика
+
+//backEnd example
+
+app.post('/users', function subscriber() {} ) // событие - пришел запрос на некий endPoint, срабатывает подписчик
+// и идет в БД за данными
+
+
+
+//subscriber === handler === observer === watcher === listener
+
+// когда мы на что-то подписываемся, обязательно должен быть метод отписки
+
+const button = {
+  subscribers: {
+    'click': [], // [subscriber1]
+    'focus': [],
+    'doubleClick' : [] // [subscriber2, subscriber3]
+  },
+  addEventListener(event, subscriber) {
+    this.subscribers[event].push(subscriber)
+  },
+  click() {
+    this.subscribers['click'].forEach((sub) => sub())
+  },
+  doubleClick() {
+    this.subscribers['doubleClick'].forEach((sub) => sub())
+  },
+  removeEventListener(event, subscriber) {
+    this.subscribers[event] = this.subscribers[event].filter((sub) => sub !== subscriber)
+  }
+}
+
+button.addEventListener('click', function subscriber1() {} )
+button.addEventListener('doubleClick', function subscriber2() {} )
+button.addEventListener('doubleClick', function subscriber3() {} )
+
+
+const sub = () => {
+  console.log('sub')
+}
+
+button.addEventListener('click', sub)
+button.removeEventListener('click', sub)
+
+// чтобы отписка сработала, важно выносить функцию в отдельную переменную, чтобы на неё была одна и та же ссылка
+// в подписке и отписке
